77be460bc2eb163a64330a479156c88cabe20c5b
Praveen <lunavathpraveen36@gmail.com>
2025-07-08 22:24:25 +0530

    Final changes before deployment

--- index.js
+++ index.js
@@ -25,13 +25,14 @@ app.use(cors({
   },
   credentials: true
 }));
-app.use(( res, next) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://lostfound-api.netlify.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
+
 
 
 
